bd1087ce19b8c34263e2aec1833b2aaf44b0d126
Aldo Gamaliel Estrada Tepec <aldoget1992@gmail.com>
2016-07-14 02:18:00 -0500

    inicio

---
+++ README.md
@@ -0,0 +1,13 @@
+# Descripcion
+Prueba de inicio y bienvenida de una app
+##Inicio
+
+![Alt "pantalla principal"](screenshot/img1.png)
+![Alt "pantalla principal"](screenshot/img2.png)
+![Alt "pantalla principal"](screenshot/img3.png)
+
+
+###Social
+- Twitter [@AldoGamaliel](https://twitter.com/AldoGamaliel)
+
+- Google+ [+Aldo Estrada](https://plus.google.com/u/0/+AldoEstrada1992)
--- app/src/main/java/com/aldo/aget/testwelcome/TestWelcomeActivity.java
+++ app/src/main/java/com/aldo/aget/testwelcome/TestWelcomeActivity.java
@@ -17,10 +17,10 @@ public class TestWelcomeActivity extends WelcomeActivity {
                 .defaultTitleTypefacePath("Montserrat-Bold.ttf")
                 .defaultHeaderTypefacePath("Montserrat-Bold.ttf")
                 .defaultBackgroundColor(R.color.default_background_color)
-                .titlePage(R.drawable.photo, "Title",R.color.blue_background)
-                .basicPage(R.drawable.photo, "Header", "More text.", R.color.colorAccent)
-                .basicPage(R.drawable.photo, "Lorem ipsum", "dolor sit amet.",R.color.orange_background)
-                .parallaxPage(R.layout.parallax, "Easy parallax", "Supply a layout and parallax effects will automatically be applied", R.color.purple_background, 0.2f, 2f)
+                .titlePage(R.drawable.photo, "Titulo",R.color.blue_background)
+                .basicPage(R.drawable.photo, "Una cabecera", "Texto aleatorio.", R.color.colorAccent)
+                .basicPage(R.drawable.photo, "Otras cosas", "Mas texto.",R.color.orange_background)
+                .parallaxPage(R.layout.parallax, "Simple parallax", "Texto informativo ...", R.color.purple_background, 0.2f, 2f)
                 .swipeToDismiss(true)
                 .build();
     }
